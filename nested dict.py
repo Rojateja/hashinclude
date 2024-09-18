738848165ef7e580eba9ed2dcc3329e21804ce4a
Rojateja <rojar2608@gmail.com>
2024-09-19 05:15:03 +0530

    Add files via upload

---
+++ nested dict.py
@@ -0,0 +1,11 @@
+myclass = {
+    "student1" : {
+        "name": "Roja",
+        "age" :'a'
+        },
+    "student2" :{
+        "name" :"Teja",
+        "age":'b'
+        }
+    }
+print(myclass)
